f3fa44b4936ca21028dc338fb347736d55a9e9cb
LotusBro98 <lotusbro98@yandex.ru>
2020-03-12 23:56:32 +0300

    Initial commit

---
+++ videosplit.py
@@ -0,0 +1,49 @@
+import os
+import sys
+sys.path.insert(0, "venv/Lib/site-packages")
+
+import ffmpeg
+import pandas as pd
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+def split_multithread(in_filename, out_filename, n_splits):
+    os.makedirs(os.path.dirname(out_filename), exist_ok=True)
+
+    probe = ffmpeg.probe(in_filename)
+    duration = float(probe["format"]["duration"])
+
+    ps = []
+
+    for i in range(0, n_splits):
+        start = i * duration / n_splits
+        end = (i + 1) * duration / n_splits
+
+        ps += [
+            ffmpeg
+            .input(in_filename)
+            .trim(start=start, end=end)
+            .output(out_filename.format(i))
+            .run_async(pipe_stdin=True)
+        ]
+
+    for p in ps:
+        p.communicate(input="y".encode())
+
+    for p in ps:
+        p.wait()
+
+if __name__ == '__main__':
+
+    root = Tk()
+    root.withdraw()  # hide the window
+    filename = askopenfilename(
+        parent=root,
+        title='Select Excel table with split defs',
+        filetypes=[('Excel table', '.xlsx'), ('Excel table', '.xls')])
+    root.destroy()
+
+    dfs = pd.read_excel(filename)
+    for index, row in dfs.iterrows():
+        input, output, n_splits = row["Input"], row["Output"], row["N splits"]
+        split_multithread(in_filename=input, out_filename=output, n_splits=n_splits)
