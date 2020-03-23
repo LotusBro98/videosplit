44acc49fcc7f25f3dba5179278ad0d0bda447148
LotusBro98 <lotusbro98@yandex.ru>
2020-03-23 21:49:36 +0300

    Changed input format

--- videosplit.py
+++ videosplit.py
@@ -1,26 +1,69 @@
+import datetime
+import glob
 import os
 import sys
 sys.path.insert(0, "venv/Lib/site-packages")
 
 import ffmpeg
-import pandas as pd
+import xlrd
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-def split_multithread(in_filename, out_filename, start, end):
+def extract_part(in_filename, out_filename, start, end):
     os.makedirs(os.path.dirname(out_filename), exist_ok=True)
 
-    p = (
-        ffmpeg
-        .input(in_filename)
+    input = ffmpeg.input(in_filename)
+    video = (
+        input
         .trim(start=start, end=end)
-        .output(out_filename)
-        .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
+        .setpts("PTS-STARTPTS")
     )
+    output = ffmpeg.output(video, out_filename)
+
+    proc = output.run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
 
     print("{}({}s : {}s) ==> {}".format(in_filename, start, end, out_filename))
-    p.communicate(input="y".encode())
-    p.wait()
+    proc.communicate(input="y".encode())
+    proc.wait()
+
+def parse_file(filename, in_dir="in", out_dir="out"):
+    wb = xlrd.open_workbook(filename)
+    sheet = wb.sheet_by_index(0)
+
+    scenes = sheet.row_values(2, 4)
+    scene_names = sheet.row_values(0, 4)[::2]
+    n_scenes = len(scene_names)
+
+    t0 = datetime.datetime(1899, 12, 31, 0, 0)
+    scenes = [(
+        scene_names[i],
+        (xlrd.xldate_as_datetime(scenes[2*i], datemode=0) - t0).total_seconds() / 60,
+        (xlrd.xldate_as_datetime(scenes[2*i+1], datemode=0) - t0).total_seconds() / 60
+    ) for i in range(n_scenes)]
+
+    starts = sheet.col_slice(3, 2)
+    starts = [(xlrd.xldate_as_datetime(t.value, datemode=0) - t0).total_seconds() / 60 for t in starts]
+
+    names = sheet.col_values(1, 2)
+
+    splits = []
+
+    for start_zero, name in zip(starts, names):
+        name_in = glob.glob(os.path.join(in_dir, name + ".*"))
+        if len(name_in) == 0:
+            raise FileNotFoundError("Input video \"{}.*\" is not found in directory \"{}\"".format(name, in_dir))
+        name_in = name_in[0]
+
+        for scene_name, start, end in scenes:
+            name_out = "{} - {}.mp4".format(name, scene_name)
+            name_out = os.path.join(out_dir, name, name_out)
+            start += start_zero
+            end += start_zero
+
+            splits.append((name_in, name_out, start, end))
+
+    return splits
+
 
 if __name__ == '__main__':
 
@@ -28,11 +71,11 @@ if __name__ == '__main__':
     root.withdraw()  # hide the window
     filename = askopenfilename(
         parent=root,
-        title='Select Excel table with split defs',
+        title='Выберите файл с диапазонами для нарезки',
         filetypes=[('Excel table', '.xlsx'), ('Excel table', '.xls')])
     root.destroy()
 
-    dfs = pd.read_excel(filename)
-    for index, row in dfs.iterrows():
-        input, output, start, end = row["Input"], row["Output"], row["Start"], row["End"]
-        split_multithread(input, output, start, end)
+    splits = parse_file(filename)
+
+    for input, output, start, end in splits:
+        extract_part(input, output, start, end)
